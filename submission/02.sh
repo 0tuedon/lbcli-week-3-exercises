cfa7210fb58000f9ffee5560d9ffa1dc7e518961
0tuedon <tuedonmofe@gmail.com>
2026-03-30 20:27:53 +0100

    feat: 01.sh -> 04.sh

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # Create a native segwit address and get the public key from the address.
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" p2sh-segwit)
+bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo $ADDRESS | jq -r ".pubkey"
+
